c37790877e59776cfaef46f9afedc623584b5bd3
Peter van den Heuvel <pvdheuvel81@gmail.com>
2020-05-05 12:47:27 +0200

    simpele veranderingen

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var header = document.querySelector('.navbar');
+var hoogte_header = header.offsetTop;
+var logo = document.querySelector('img.logo');
+
+window.onscroll = function () {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
+    if (window.pageYOffset > hoogte_header) {
+        header.classList.add("stick-to-top");
+        logo.src = "./img/taste-it-black.svg";
+        //header.style.backgroundColor = 'white';
+    } else {
+        header.classList.remove("stick-to-top");
+        logo.src = "./img/taste-it-white.svg";
+    }
+};
+
+// var images = document.querySelectorAll('.img-parallax');
+// // documentation: https://simpleparallax.com/#example-scale
+// new simpleParallax(image, {
+//     scale: 1.2
+// });
